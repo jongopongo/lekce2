f4fb8c93489871295bf10fc5f7db7440d07f072d
Klára <pepsinka@gmail.com>
2024-05-02 20:59:05 +0200

    lekce 5

---
+++ lekce 5/script.js
@@ -0,0 +1,17 @@
+const elNadpis = document.querySelector("h1")
+const elText = document.querySelector (".modre-pozadi")
+const elText3= document.querySelector("#text3")
+const elText5 = document.querySelector("#text5")
+
+elNadpis.textContent = "Prepsal me JS"
+elNadpis.style.color = "gold"
+elNadpis.style.background = "deeppink"
+elNadpis.style.fontFamily = "Arial"
+
+elText.textContent = "Prepsal me JS"
+elText3.textContent = "Pizza hawaii je nejlepsi"
+elText5.textContent = "JS 1 od Czechitas je super"
+console.log(elNadpis)
+
+
+
